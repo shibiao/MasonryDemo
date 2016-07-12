8cff9bd48095c6d4107155fd97b75273c4e2e603
sycf ios <sycf_ios_13579@sycf-iosdeimac.local>
2016-07-12 17:32:00 +0800

    Initial Commit

---
+++ MasonryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryDemo
+//
+//  Created by sycf_ios on 16/7/12.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasonryDemo
+//
+//  Created by sycf_ios on 16/7/12.
+//  Copyright © 2016年 sycf_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
